2c942497284caa265949530215cfd32fa6b95a57
amirismailov1 <amirismailov122@gmail.com>
2022-05-08 19:49:21 +0600

    'test2'

--- src/redux/reducers/set/set.js
+++ src/redux/reducers/set/set.js
@@ -21,7 +21,7 @@ export default (state = initialState, action) => {
 
 export const getSet = () => {
     return (dispatch) => {
-        axios('http://localhost:8080/set')
+        axios('https://yabao.vercel.app/api/set')
             .then(({data}) => {
                 return dispatch({type:'GETSET', arr : data})
             })
